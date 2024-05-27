ffd381a72a5c6951a64bfc596b2416867c53853a
Tillian <tdhume@laposte.net>
2024-05-27 09:57:13 +0200

    Louise

--- src/main/java/com/example/demo/controller/LivreRestController.java
+++ src/main/java/com/example/demo/controller/LivreRestController.java
@@ -1,5 +1,32 @@
 package com.example.demo.controller;
 
-public class LivreRestController {
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.model.Livre;
+import com.example.demo.model.Produit;
+import com.example.demo.repository.LivreRepository;
+
+@CrossOrigin(origins = "http://127.0.0.1:4200")
+@RestController
+public class LivreRestController {
+	@Autowired LivreRepository livreRepository;
+	
+	@GetMapping("/livre")
+    public List<Livre> getProduit() {
+        return livreRepository.findAll();
+    }
+	
+	@PostMapping("/livre")
+	public Livre addOrUpdateLivre (@RequestBody Livre livre) {
+		livreRepository.save(livre);
+		return livre;
+	}
 }
--- src/main/java/com/example/demo/controller/ProduitRestController.java
+++ src/main/java/com/example/demo/controller/ProduitRestController.java
@@ -3,6 +3,7 @@ package com.example.demo.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -13,10 +14,11 @@ import org.springframework.web.bind.annotation.RestController;
 import com.example.demo.model.Produit;
 import com.example.demo.repository.ProduitRepository;
 
+@CrossOrigin(origins = "http://127.0.0.1:4200")
 @RestController
 public class ProduitRestController {
 	@Autowired ProduitRepository produitRepository;
-    
+	
     @GetMapping("/produit")
     public List<Produit> getProduit() {
         return produitRepository.findAll();
