4b610dc9d7bb5b24bbc2009ec392f7692f584165
Tillian <tdhume@laposte.net>
2024-06-02 17:08:12 +0200

    Louise

--- src/main/java/com/example/demo/controller/ProduitController.java
+++ src/main/java/com/example/demo/controller/ProduitController.java
@@ -1,14 +1,20 @@
 package com.example.demo.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.repository.ProduitRepository;
+
 
 @Controller
 public class ProduitController {
-	
-	@GetMapping("/produit")
-	public String afficheProduit() {
+	@Autowired ProduitRepository produitRepository;
+
+	@GetMapping("/getDvd/{idBiblio}")
+	public String affiche(@PathVariable("id") String id) {
 		return "produit";
 	}
 }
